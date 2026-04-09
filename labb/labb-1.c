8d0dd5929ee61fc92b621c79401afffc3e2fc7c5
Love Danielsson <ldn25003@student.mdu.se>
2026-04-09 11:27:50 +0200

    basic code structure

---
+++ labb/labb-1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Exorcise 1
+void read_string(char* buffer, size_t bufferSize) {
+
+}
+int read_int() {
+    int number;¨
+
+    return number;
+}
+void send_int(int number) {
+
+}
+
+// Exorcise 2
+void lightLEDWithButton() {
+
+}
+
+// Exorcise 3
+void delay_s(int seconds) {
+
+}
+
+// Exorcise 4
+void init_rng() {
+    
+}
---
+++ labb/labb-1.h
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifndef LABB_1
+#define LABB_1
+
+// Exorcise 1
+void read_string(char* buffer, size_t bufferSize);
+int read_int();
+void send_int(int number);
+
+// Exorcise 2
+void lightLEDWithButton();
+
+// Exorcise 3
+void delay_s(int seconds);
+
+// Exorcise 4
+void init_rng();
+
+#endif
