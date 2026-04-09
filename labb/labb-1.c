8144f5d0f8dee75d8f2aaadf4afac569ddd7989a
Love Danielsson <ldn25003@student.mdu.se>
2026-04-09 11:44:25 +0200

    uppgift 1.1

--- labb/labb-1.c
+++ labb/labb-1.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <nrfx_uarte.h>
 
 // Exorcise 1
-void read_string(char* buffer, size_t bufferSize) {
+char getchar() {
+    char* c;
+    nrfx_uarte_rx (&instance, c, sizeof(char));
+    uarte_write(c, sizeof(char));
+    return *c;
+}
+#define RETURN_CHAR '\r'
+void read_string(char* buffer, size_t bufferSize) {{
+    int i;
+    buffer[0] = getchar();
+    // Read characters until the output buffer is full or a the return character character is given
+    for (i = 1; output[i - 1] != RETURN_CHAR && i < bufferSize; i++)
+        buffer[i] = getchar();
+
+    // If the output buffer was filled and the last character was not the return character,
+    // then let the user enter characthers untill a return character
+    if (i == bufferSize && output[i - 1] != RETURN_CHAR)
+        while (getchar() != RETURN_CHAR); 
+
+    // Terminate the string
+    buffer[i - 1] = '\0';
+}
 
 }
 
