fe5f74c77569d59eebec85e1e29acb493185a604
Viggo Sörensen <vsn25006@student.mdu.se>
2026-04-10 11:43:50 +0200

    Fixed errors with makefile and other things. Also finished ex 2

--- labb/labb-1.c
+++ labb/labb-1.c
@@ -5,6 +5,9 @@
 #include <nrfx_config.h>
 #include <nrf.h>
 #include <nrfx_uarte.h>
+#include <nrfx_systick.h>
+#include <nrf_gpio.h>
+#include "labb-1.h"
 
 #define LED1 28
 #define LED2 29 
@@ -12,15 +15,16 @@
 #define LED4 31
 #define BUTTON1 23
 #define BUTTON2 24 
-#define BUTTON1 08
-#define BUTTON2 09
+#define BUTTON3 8
+#define BUTTON4 9
 #define LED_OFF 1
-#define LE_DON 0 
+#define LED_ON 0 
 
 
 // Exorcise 1
 void read_string(char* buffer, size_t bufferSize) {
 
+    return;
 }
 
 int read_int() {
@@ -30,13 +34,13 @@ int read_int() {
 }
 
 void send_int(int number) {
-
+    return;
 }
 
 // Exorcise 2
 void lightLEDWithButton() {
 
-    nrfx_systic_init();
+    nrfx_systick_init();
 
     nrf_gpio_cfg_output(LED1);
     nrf_gpio_cfg_output(LED2);
@@ -49,39 +53,41 @@ void lightLEDWithButton() {
     nrf_gpio_pin_write(LED3, LED_OFF);
     nrf_gpio_pin_write(LED4, LED_OFF);
 
-    nrf_gpio_cfg_input(BUTTON1, NRF_GPIO-PIN_PULLUP);
-    nrf_gpio_cfg_input(BUTTON2, NRF_GPIO-PIN_PULLUP);
-    nrf_gpio_cfg_input(BUTTON3, NRF_GPIO-PIN_PULLUP);
-    nrf_gpio_cfg_input(BUTTON4, NRF_GPIO-PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON1, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON2, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON3, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(BUTTON4, NRF_GPIO_PIN_PULLUP);
 
     while(1)
     {
-        nrf_systick_delay_ms(50);
-
+        nrfx_systick_delay_ms(50);
+        
         if (nrf_gpio_pin_read(BUTTON1) == 0){
-            nrf_gpio_pin_write(LED1,LED_ON)
-        }else {nrf_gpio_pin_write(LED1, LED_OFF)}
+            nrf_gpio_pin_write(LED1,LED_ON);
+        }else {nrf_gpio_pin_write(LED1, LED_OFF);}
 
         if (nrf_gpio_pin_read(BUTTON2) == 0){
-            nrf_gpio_pin_write(LED2,LED_ON)
-        }else {nrf_gpio_pin_write(LED2, LED_OFF)}
+            nrf_gpio_pin_write(LED2,LED_ON);
+        }else {nrf_gpio_pin_write(LED2, LED_OFF);}
 
         if (nrf_gpio_pin_read(BUTTON3) == 0){
-            nrf_gpio_pin_write(LED3,LED_ON)
-        }else {nrf_gpio_pin_write(LED3, LED_OFF)}
+            nrf_gpio_pin_write(LED3,LED_ON);
+        }else {nrf_gpio_pin_write(LED3, LED_OFF);}
 
-        if (nrf_gpio_pin_read(BUTTON3) == 0){
-            nrf_gpio_pin_write(LED1,LED_ON)
-        }else {nrf_gpio_pin_write(LED3, LED_OFF)}
+        if (nrf_gpio_pin_read(BUTTON4) == 0){
+            nrf_gpio_pin_write(LED4,LED_ON);
+        }else{nrf_gpio_pin_write(LED4, LED_OFF);}
     }
 }
 
 // Exorcise 3
 void delay_s(int seconds) {
 
+    return;
 }
 
 // Exorcise 4
 void init_rng() {
     
+    return;
 }
