a7bb96d3e7c6335df882da4cbfe96655ee3d8bfd
Love Danielsson <ldn25003@student.mdu.se>
2026-04-10 10:22:15 +0200

    bug fixes

--- labb/labb-1.c
+++ labb/labb-1.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <nrfx_uarte.h>
 #include <nrfx_rtc.h>
 
@@ -17,24 +18,27 @@ void uarte_init() {
         //Här kan vi implementera felhantering, men för stunden håller vi tummarna att allt funkar
     }
 }
-char getchar() {
-    char* c;
-    nrfx_uarte_rx (&uarte_instance, c, sizeof(char));
-    nrfx_uarte_tx(&uarte_instance, c, sizeof(char), 0);
+void uarte_write(uint8_t* string, size_t stringLength) {
+    nrfx_uarte_tx(&uarte_instance, string, stringLength, 0);
+}
+uint8_t getchar() {
+    uint8_t* c;
+    nrfx_uarte_rx (&uarte_instance, c, sizeof(uint8_t));
+    uarte_write(c, sizeof(uint8_t));
     return *c;
 }
 #define RETURN_CHAR '\r'
 #define BUFFER_OVERFLOW_ERROR_MESSAGE " (inputed string was longer that the buffer size)"
-void read_string(char* buffer, size_t bufferSize) {
+void read_string(uint8_t* buffer, size_t bufferSize) {
     int i;
     buffer[0] = getchar();
     // Read characters until the output buffer is full or a the return character character is given
-    for (i = 1; output[i - 1] != RETURN_CHAR && i < bufferSize; i++)
+    for (i = 1; buffer[i - 1] != RETURN_CHAR && i < bufferSize; i++)
         buffer[i] = getchar();
 
     // If the output buffer was filled and the last character was not the return character,
     // then let the user enter characthers untill a return character
-    if (i == bufferSize && output[i - 1] != RETURN_CHAR) {
+    if (i == bufferSize && buffer[i - 1] != RETURN_CHAR) {
         while (getchar() != RETURN_CHAR); 
         uarte_write(BUFFER_OVERFLOW_ERROR_MESSAGE, sizeof(BUFFER_OVERFLOW_ERROR_MESSAGE));
     }
@@ -45,7 +49,7 @@ void read_string(char* buffer, size_t bufferSize) {
 
 #define NUMBER_STRING_BUFFER_SIZE 32
 int read_int() {
-    char stringOfNum[NUMBER_STRING_BUFFER_SIZE];
+    uint8_t stringOfNum[NUMBER_STRING_BUFFER_SIZE];
 
     read_string(stringOfNum, NUMBER_STRING_BUFFER_SIZE);
 
@@ -53,9 +57,9 @@ int read_int() {
 }
 
 void send_int(int number) {
-    char stringOfNum[NUMBER_STRING_BUFFER_SIZE];
+    uint8_t stringOfNum[NUMBER_STRING_BUFFER_SIZE];
     
-    stringOfNum = sprintf("%i", number);
+    sprintf("%d", number, stringOfNum);
     uarte_write(stringOfNum, sizeof(stringOfNum));
 }
 
@@ -67,7 +71,7 @@ void lightLEDWithButton() {
 // Exorcise 3
 const nrfx_rtc_t rtc_instance = NRFX_RTC_INSTANCE(0);
 void rtc_init() {
-    const nrfx_rtc_config_t config = NRFX_RTC_DEFAULT_CONFIG();
+    const nrfx_rtc_config_t config = NRFX_RTC_DEFAULT_CONFIG;
 
     //Initierar RTC:
     nrfx_err_t errr = nrfx_rtc_init(&rtc_instance, &config, NULL);
