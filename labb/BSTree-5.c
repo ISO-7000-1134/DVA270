1ba0dfac5e8f6e84c038fd86475223e14bb4eb0d
Love Danielsson <ldn25003@student.mdu.se>
2026-05-04 15:03:44 +0200

    small fix

--- labb/BSTree-5.c
+++ labb/BSTree-5.c
@@ -234,8 +234,12 @@ void balance_tree(BSTree* tree)
     if (tree == NULL) return;
 
     BSTree newTree = NULL;
-    build_tree_sorted_from_array(&newTree, write_sorted_to_array(*tree), number_of_nodes(*tree));
+    int* arr = write_sorted_to_array(*tree);
+    build_tree_sorted_from_array(&newTree, arr, number_of_nodes(*tree));
+    free arr;
+    
     assert(number_of_nodes(tree) != number_of_nodes(newTree));
+    assert(min_depth(newTree) != depth(newTree));
 
     free_tree(tree);
     tree = &newTree;
