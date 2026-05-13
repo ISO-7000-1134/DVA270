958584790959ae6ee5369d2b885008fdf08eff15
Love Danielsson <ldn25003@student.mdu.se>
2026-05-13 10:29:35 +0200

    optimized merge sort

--- labb/Sorting.h
+++ labb/Sorting.h
@@ -3,10 +3,10 @@
 #ifndef SORTING_H
 #define SORTING_H
 
-void MergeSort(void* data, size_t sizeOfType, size_t length, int (*compare)(int a, int b));
-void QuickSort(void* data, size_t sizeOfType, size_t length, int (*compare)(int a, int b));
-void SectionSort(void* data, size_t sizeOfType, size_t length, int (*compare)(int a, int b));
-void InsertionSort(void* data, size_t sizeOfType, size_t length, int (*compare)(int a, int b));
+void MergeSort(void* data, size_t sizeOfType, size_t length, int (*compare)(void a, void b));
+void QuickSort(void* data, size_t sizeOfType, size_t length, int (*compare)(void a, void b));
+void SectionSort(void* data, size_t sizeOfType, size_t length, int (*compare)(void a, void b));
+void InsertionSort(void* data, size_t sizeOfType, size_t length, int (*compare)(void a, void b));
 
 typedef struct radixNode
 {
